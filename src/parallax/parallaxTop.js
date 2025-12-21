b21426c54e036f7d5c93ae0a259c6a5103cb7287
John Vince Berjuega <adepti.jvberjuega@gmail.com>
2025-12-22 01:15:28 +0800

    Optimized the Scrolls and Gaps, Added second part, fixed thirt part

--- src/parallax/parallaxTop.js
+++ src/parallax/parallaxTop.js
@@ -2,24 +2,24 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 export default function ParallaxTop() {
   return (
-    <Parallax pages={7} style={{ top: "0", left: "0" }} class="animation">
-      <ParallaxLayer offset={0} speed={0.4} zIndex={4}>
+    <Parallax pages={5} style={{ top: "0", left: "0" }} class="animation">
+      <ParallaxLayer offset={0} speed={0.4} zIndex={200}>
         <div className="animation_layer parallax" id="PurpleBG"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={1} zIndex={5}>
+      <ParallaxLayer offset={0} speed={1} zIndex={210}>
         <div className="animation_layer parallax" id="Moon"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={1} zIndex={6}>
+      <ParallaxLayer offset={0} speed={1} zIndex={210}>
         <div className="animation_layer parallax" id="Cloud"></div>
       </ParallaxLayer>
 
       <ParallaxLayer
         offset={0}
         speed={0.6}
-        style={{ zIndex: "999" }}
-        className="w-screen text-center pointer-events-none"
+        style={{ zIndex: "220" }}
+        className="w-screen text-center"
       >
         <div className="animation_layer parallax flex">
           <div className="TopParallaxPanel pt-10">
@@ -37,20 +37,20 @@ export default function ParallaxTop() {
                 href="https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-blue-500 underline cursor-pointer flex justify-center pt-2 pointer-events-auto"
+                className="text-blue-500 underline cursor-pointer flex justify-center pt-2 pointer-events-auto "
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  strokeWidth={1.5}
+                  stroke-width="1.5"
                   stroke="currentColor"
-                  className="size-6"
+                  class="size-10 p-1 transition-colors hover:bg-zinc-500/50 transition-all duration-100 rounded-3xl m-0 p-1 pointer-events-auto"
                 >
                   <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                   />
                 </svg>
               </a>
@@ -60,64 +60,122 @@ export default function ParallaxTop() {
         </div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.4} zIndex={6}>
+      <ParallaxLayer offset={0} speed={0.4} zIndex={230}>
         <div className="animation_layer parallax" id="Layer1"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.4} zIndex={7}>
+      <ParallaxLayer offset={0} speed={0.4} zIndex={240}>
         <div className="animation_layer parallax" id="Layer2"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.5} zIndex={8}>
+      <ParallaxLayer offset={0} speed={0.5} zIndex={250}>
         <div className="animation_layer parallax" id="Layer3"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.4} zIndex={9}>
+      <ParallaxLayer offset={0} speed={0.5} zIndex={250}>
+        <div className="animation_layer parallax" id="Twigs"></div>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={0.4} zIndex={300}>
         <div className="animation_layer parallax" id="FillerClouds"></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.5} zIndex={9}>
+      <ParallaxLayer offset={0} speed={0.5} zIndex={400}>
         <div className="animation_layer parallax" id="Character"></div>
       </ParallaxLayer>
       <div class="spacer"></div>
 
-
-
-
-      <ParallaxLayer offset={1} speed={0.4} zIndex={2}>
-        <div className="animation_layer parallax" id="Raiden"></div>
+      <ParallaxLayer offset={1.6} speed={0.6} zIndex={110}>
+        <div
+          className="animation_layer parallax brightness-[0.3]"
+          id="DreamSakura"
+        ></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={1} speed={0.4} zIndex={99}>
-        <div className="animation_layer parallax" id="DreamSakura" style={{ backgroundColor: 'red' }}></div>
+      <ParallaxLayer offset={1.6} speed={0.3} zIndex={130}>
+        <div
+          className="animation_layer parallax brightness-[0.3]"
+          id="Raiden"
+        ></div>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={1} speed={0.5} zIndex={10}>
-        <div className="animation_layer parallax" id=""></div>
+      <ParallaxLayer offset={1.6} speed={0.3} zIndex={130}>
+        <div className="flex flex-col justify-center items-center text-white w-full h-full px-10">
+          <p className="text-5xl font-black shadows-into-light-regular mb-10">
+            Raiden Builds
+          </p>
+
+          <div className="grid grid-cols-3 xl:grid-cols-4 gap-8 w-full max-w-7xl">
+            {[
+              "Hyper Carry",
+              "National",
+              "Hyperbloom",
+              "Overload",
+              5,
+              6,
+              7,
+              8,
+            ].map((item) => (
+              <div
+                key={item}
+                className="bg-black/40 backdrop-blur-[2px] border border-purple-500/30 p6 rounded-lg hover:border-purple-500 transition-colors h-48 flex items-center justify-center"
+              >
+                <span className="text-xl font-bold">Team {item} </span>{" "}
+              </div>
+            ))}
+          </div>
+        </div>
       </ParallaxLayer>
 
-      <ParallaxLayer
-        offset={2}
-        speed={0.6}
-        zIndex={21}
-        style={{ paddingTop: "200px" }}
-      >
-        <div className="animation_layer parallax" id="">
-          <div id="textblock">
-            <div id="textblock-container">
-              <h1 id="textblock-title">Sample Text?</h1>
-              <p id="textblock-content">
-                lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. <br />
-                <br />
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. <br />
-                <br />
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia deserunt mollit anim id est laborum.
+      <ParallaxLayer offset={3} speed={0.6} zIndex={300}>
+        <div className="animation_layer parallax px-40" id="">
+          <div id="textblock w-100 h-100">
+            <div id="textblock-container flex justify-center items-center text-center">
+              <h1
+                className="text-center text-purple-500 mb-4"
+                id="textblock-title"
+              >
+                The Electro Archons Might
+              </h1>
+              <p
+                className="indent-10 text-violet leading-relaxed font-serif"
+                id="textblock-content"
+              >
+                The Raiden Shogun is the supporter of "Eternity," the undisputed
+                ruler of Inazuma who seeks to rid the world of fleeting desires
+                and disturbances. As the vessel of Beelzebul, she manifests as
+                both the robotic Shogun and the meditating Ei within the Plane
+                of Euthymia. Following the loss of her twin sister, Makoto,
+                during the cataclysm five hundred years ago, Ei relinquished her
+                physical form to reside in a meditative sub-space, creating a
+                puppet to govern the nation in her stead.
               </p>
+              <br />
+              <p
+                className="indent-10 text-violet leading-relaxed font-serif"
+                id="textblock-content"
+              >
+                Since the dawn of the Shogunate, she has pursued a realm where
+                nothing is lost and all remains unchanging. This singular focus
+                led to the implementation of the Vision Hunt Decree and the
+                Sakoku Decree, isolating Inazuma from the rest of Teyvat to
+                prevent the erosion of her people's dreams and the stability of
+                the land.
+              </p>
+              <br />
+
+              <p
+                className="indent-10 text-violet leading-relaxed font-serif"
+                id="textblock-content"
+              >
+                With the Musou no Hitotachi, she punishes those who threaten the
+                heavenly principles, severing the threads of fate to preserve
+                the stillness of Inazuma for all eternity. Only through the
+                clash of ideals and the reawakening of her people's ambitions
+                has she begun to reconsider the true nature of eternity—not as a
+                stagnant freeze in time, but as a promise kept through the ages.
+              </p>
+              <br />
             </div>
             <footer id="textblock-footer">
               <a id="textblock-text"> </a>
--- src/templates/navigationBar.js
+++ src/templates/navigationBar.js
@@ -1,10 +1,10 @@
 export default function TopBar() {
   return (
-    <div className="w-full bg-gray shadow-lg TopBarNav">
-      <nav className="fixed top-0 max-w-full mx-auto px-20px sm:px-10 lg:px-20 pt-5 w-full">
+    <div className="w-full shadow-lg TopBarNav">
+      <nav className="fixed top-0 max-w-full mx-auto sm:px-10 lg:px-20 w-full">
         <div className="flex items-center justify-between h-16 group">
           {/* Logo/Brand Name */}
-          <div className="text-xl font-bold text-white tracking-wider">
+          <div className="font-bold text-white tracking-wider">
           Electro Archon
           </div>
 
@@ -13,7 +13,7 @@ export default function TopBar() {
             <li className="group">
               <a
                 href="#home"
-                className="neon-button rounded-2xl px-3 py-2 text-md text-xl transition duration-150"
+                className="neon-button rounded-2xl px-3 py-2 text-md transition duration-50"
               >
                 Home
               </a>
@@ -21,23 +21,23 @@ export default function TopBar() {
             <li className="group">
               <a
                 href="#about"
-                className="neon-button rounded-2xl px-3 py-2 text-md text-xl transition duration-150"
+                className="neon-button rounded-2xl px-3 py-2 text-md transition duration-50"
               >
-                About
+                Teams
               </a>
             </li>
             <li className="group">
               <a
                 href="#services"
-                className="neon-button rounded-2xl px-3 py-2 text-md text-xl transition duration-150"
+                className="neon-button rounded-2xl px-3 py-2 text-md transition duration-50"
               >
-                Settings?
+                Lore
               </a>
             </li>
             <li className="group">
               <a
                 href="#contact"
-                className="neon-button px-3 py-2 rounded-2xl text-md text-xl transition duration-150"
+                className="neon-button px-3 py-2 rounded-2xl text-md transition duration-50"
               >
                 Contact
               </a>
